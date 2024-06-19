15212489a118c571b2ab68236a327748fe35b61c
evertongodoy <everton.mgodoy@gmail.com>
2024-06-19 09:43:11 -0300

    first commit
    Adding the first project files

---
+++ src/main/java/com/senac/restful_db/RestfulDbApplication.java
@@ -0,0 +1,13 @@
+package com.senac.restful_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulDbApplication.class, args);
+	}
+
+}
