2f83235a2a4353a4713fceb2b7842aba575514bc
Everton Godoy <everton.mgodoy@gmail.com>
2024-06-20 14:34:42 -0300

    add config
    add config database and adjusts to run the application using flyway

--- src/test/java/com/senac/restful_db/RestfulDbApplicationTests.java
+++ src/test/java/com/senac/restful_db/RestfulDbApplicationTests.java
@@ -1,13 +1,13 @@
-package com.senac.restful_db;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class RestfulDbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.senac.restful_db;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class RestfulDbApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
